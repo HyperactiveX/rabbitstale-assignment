2ce6af480449fe4552ec44668f38be9744d9a934
HyperactiveX <s.chalanthorn@gmail.com>
2022-03-26 16:05:25 +0700

    [FIX] : Global Styling

--- rabbit-app/src/index.tsx
+++ rabbit-app/src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Todo from './todo-page';
+import './index.css'
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Todo />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
