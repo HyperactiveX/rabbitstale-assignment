fe7200609b8de9e7d0573998649d7328edbccd4f
HyperactiveX <s.chalanthorn@gmail.com>
2022-03-27 11:38:43 +0700

    [ADD] : Add sumResult State

--- rabbit-app/src/page/index.tsx
+++ rabbit-app/src/page/index.tsx
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from "react";
 import styles from "./Index.module.css";
 import AddElement from "../components/AddElement/AddElement";
 import ShowTodoList from "../components/ShowList/ShowTodoList";
-import { ToDoType } from "../types/Types";
+import { SumResultType, ToDoType } from "../types/Types";
 
 function TodoList() {
 	const [toDoList, setToDoList] = useState<ToDoType[]>([
 		{ title: "", state: false },
 	]);
+	const [sumResult, setSumResult] = useState<SumResultType>({
+		number: 0,
+		string: "",
+	});
 
 	const renderTodoList = () => {
 		return toDoList.map((element, key) => {
@@ -22,15 +26,20 @@ function TodoList() {
 		});
 	};
 
-	// useEffect(() => {
-	// 	renderTodoList();
-	// }, [toDoList]);
-
 	return (
 		<div className={styles.page}>
 			<div className={styles.addTodoList}>
 				<h1>My Todo List</h1>
-				<AddElement setList={setToDoList} getList={toDoList} />
+				<AddElement
+					setList={setToDoList}
+					getList={toDoList}
+					setSum={setSumResult}
+				/>
+				<p>Total Sum of Number: {sumResult.number}</p>
+				<p>
+					Concat of All String:{" "}
+					{sumResult.string === "" ? '" "' : sumResult.string}
+				</p>
 			</div>
 			<div className={styles.showTodoList}>{renderTodoList()}</div>
 		</div>
