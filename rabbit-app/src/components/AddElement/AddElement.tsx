657e6f609a58d4127703c59f5b7bc75879c2f9ec
HyperactiveX <s.chalanthorn@gmail.com>
2022-03-27 11:38:06 +0700

    [UPDATE] : Refactor code & Implement sum function

--- rabbit-app/src/components/AddElement/AddElement.tsx
+++ rabbit-app/src/components/AddElement/AddElement.tsx
@@ -1,40 +1,67 @@
 import React, { useState } from "react";
-import { ToDoType } from "../../types/Types";
+import { SumResultType, ToDoType } from "../../types/Types";
 import styles from "./AddElement.module.css";
 
 type Prop = {
 	getList: ToDoType[];
 	setList: React.Dispatch<React.SetStateAction<ToDoType[]>>;
+	setSum: React.Dispatch<React.SetStateAction<SumResultType>>;
 };
 
-const AddElement: React.FC<Prop> = ({ getList, setList }) => {
-	const [title, setTitle] = useState("");
-	const handleClick = (title: string) => {
+const AddElement: React.FC<Prop> = ({ getList, setList, setSum }) => {
+	const [title, setTitle] = useState<string>("");
+
+	const handleSubmit = (title: string) => {
 		if (title === "") {
 			return alert("You must write something!");
 		}
 		setList((list) => [...list, { title: title, state: false }]);
 		return setTitle("");
 	};
-	const sumAllTodo = (list: ToDoType[]) => {};
+
+	const sumAllNumber = (list: ToDoType[]) => {
+		return list
+			.map((element, index) => {
+				let title = parseInt(element.title);
+				return isNaN(title) ? 0 : title;
+			})
+			.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+	};
+
+	const concatAllString = (list: ToDoType[]) => {
+		return list
+			.map((element, index) => {
+				let title = element.title;
+				return isNaN(parseInt(title)) ? title : "";
+			})
+			.reduce((prev, curr) => prev + curr, "");
+	};
+	const sumAll = (list: ToDoType[]) => {
+		setSum({ number: sumAllNumber(list), string: concatAllString(list) });
+	};
 
 	return (
 		<div className={styles.addComponent}>
-			<input
-				type='text'
-				placeholder='Title...'
-				className={styles.inputField}
-				onChange={(e) => setTitle(e.target.value)}
-				value={title}
-			/>
-			<button
-				className={`${styles.btn} ${styles.add}`}
-				onClick={() => handleClick(title)}>
-				Add
-			</button>
+			<form
+				className={styles.formContainer}
+				onSubmit={(e) => {
+					e.preventDefault();
+					handleSubmit(title);
+				}}>
+				<input
+					type='text'
+					placeholder='Title...'
+					className={styles.inputField}
+					onChange={(e) => setTitle(e.target.value)}
+					value={title}
+				/>
+				<button className={`${styles.btn} ${styles.add}`} type='submit'>
+					Add
+				</button>
+			</form>
 			<button
 				className={`${styles.btn} ${styles.sum}`}
-				onClick={() => sumAllTodo(getList)}>
+				onClick={() => sumAll(getList)}>
 				Sum
 			</button>
 		</div>
